fd323d43ab7ce17a59204b711271d0843e424d67
Zac Butko <zac.butko@gmail.com>
2022-05-24 19:50:06 -0700

    Upgrade react and types

--- packages/esm-patient-registration-app/src/root.component.test.tsx
+++ packages/esm-patient-registration-app/src/root.component.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Root from './root.component';
 import { match } from 'react-router-dom';
 
@@ -16,7 +16,8 @@ const sampleMatchProp: match<{ patientUuid: string }> = {
 describe('root component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(
+    const root = createRoot(div);
+    root.render(
       <Root
         savePatientForm={jest.fn()}
         match={sampleMatchProp}
@@ -25,7 +26,6 @@ describe('root component', () => {
         patientIdentifiers={[]}
         relationshipTypes={{ results: [] }}
       />,
-      div,
     );
   });
 });
