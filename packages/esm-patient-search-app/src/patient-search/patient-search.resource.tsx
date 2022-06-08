8322efa78aec18b28653389325cbf82410e84b9e
Donald Kibet <chelashawdonald@yahoo.com>
2022-06-08 11:36:47 +0300

    pair program with juliet

--- packages/esm-patient-search-app/src/patient-search/patient-search.resource.tsx
+++
@@ -1,9 +0,0 @@
-import { openmrsFetch } from '@openmrs/esm-framework';
-
-export function performPatientSearch(query: string, objectVersion: string, ac: AbortController, includeDead: boolean) {
-  const url = `/ws/rest/v1/patient?q=${query}&v=${objectVersion}&includeDead=${includeDead}`;
-  return openmrsFetch(url, {
-    method: 'GET',
-    signal: ac.signal,
-  });
-}
